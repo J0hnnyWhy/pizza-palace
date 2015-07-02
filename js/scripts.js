39fc6d9a10caab9975ea889d8ea652d2a9fc3e8e
john <jayelwhy@yahoo.com>
2015-07-02 11:57:30 -0700

    include pricing for multiple pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ function Pizza(firstName, size, toppings) {
 	this.firstName = firstName;
 	this.size = size;
 	this.toppings = toppings;
+	var order = []
 
 	this.price = function() {
 	    var totalPrice = 6;
@@ -12,8 +13,18 @@ function Pizza(firstName, size, toppings) {
 	    } else if (this.size === "XL"){
 	      totalPrice += 8;
 	    }
-	    return totalPrice;
-	} 
 
+	    if (toppings.length > 0 && toppings.length < 4) {
+	    	 var toppingPrice = (toppings.length * 2)
+	    	totalPrice = totalPrice + toppingPrice
+	    } else if (toppings.length >= 4) {
+	    	toppingPrice = (toppings.length * 1.5)
+	    	totalPrice = totalPrice + toppingPrice
+	    }
+	    order.push(totalPrice)
+	    return totalPrice;	
+	} 	
 }
- 
+Pizza.prototype.finalPrice = function(quantity)  {
+  return this.price() * quantity;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -6,10 +6,16 @@ describe('Pizza', function() {
   });
 
   it("calculates the price of a pizza based on size and toppings", function() {
-    var testPizza = new Pizza("Bobby", "large", ["onions"]);
+    var testPizza = new Pizza("Bobby", "large", ["onions", "pb", "razorblade", "toothpick", "jellybean"]);
     expect(testPizza.firstName).to.equal("Bobby");
     expect(testPizza.size).to.equal("large");
-    expect(testPizza.price()).to.equal(10);
+    expect(testPizza.price()).to.equal(17.5);
   });
+
+  it("calculates the total price of all pizzas purchased", function() {
+  	var testPizza = new Pizza("Bobby", "large", ["onions", "pb", "razorblade", "toothpick", "jellybean"]);
+    expect(testPizza.price()).to.equal(17.5);
+    expect(testPizza.finalPrice(2)).to.equal(35);
+  })
 
 });
